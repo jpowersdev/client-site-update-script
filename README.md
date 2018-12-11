ed190eef280ca0acd0bcb1926da6efb297a19ad2
Ebonie <ebonie@metalandcoffee.com>
2018-12-11 12:35:25 -0500

    README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# client-site-update-script
+## Client Site Update Script
+
+Run this script from a terminal session in order to automatically update WPEngine Installs 
+
+Confirm that you are able to successfully connect to WPEngine SSH Gatweway
+* https://wpengine.com/support/getting-started-ssh-gateway/
+
+
+## Site updates not covered
+
+https://fight.org/wp-admin/
+https://activateworld.com/wp-admin/
+https://www.gbs-cidp.org/wp-admin/
+
+_More to be added..._
+
+
---
+++ plugins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+installs='yikesplugins phlcontroller dineoutforlife soulfullproj spicermullikin phlgeekawards inoutsource upennaisp delawarenature htjewelry mstonepartners actacademy alliancewe baums casapapel ccardev chop d3b decibelmag designthinkstd flyingfish harvestmkt iba kimberton lindaleealter movingt multimeasures nursepartners patatgiovannis philaerc phillymagic phrootz pjerome researchforact riverfrontn steverotfeld waverly'
+for install in $installs;
+do
+  printf "\n"
+  echo "$install@$install.ssh.wpengine.net"
+  printf "\n"
+  ssh $install@$install.ssh.wpengine.net 'cd /sites/$install; wp core update; wp plugin update --all --exclude=slider-pro,slider-pro-assets,mailgun,woocommerce; exit;'
+done
