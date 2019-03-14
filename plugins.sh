af7df30ce0809a4e82beeef8f469b0cefd4b0548
Kevin Utz <kevin@yikesinc.com>
2019-03-14 09:54:14 -0400

    Printing out some stars in console while the script is running :)

--- plugins.sh
+++ plugins.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "**********************************************************************"
+echo "******************** Running YIKES Update Script *********************"
+echo "**********************************************************************"
+printf "\n"
+
 installs='yikesplugins phlcontroller dineoutforlife soulfullproj
 		spicermullikin phlgeekawards inoutsource upennaisp
 		delawarenature htjewelry mstonepartners actacademy
@@ -9,10 +14,18 @@ installs='yikesplugins phlcontroller dineoutforlife soulfullproj
 		philaerc phillymagic phrootz pjerome
 		researchforact riverfrontn steverotfeld waverly
 		tredyffrin'
+
 for install in $installs;
 do
-  printf "\n"
-  echo "$install@$install.ssh.wpengine.net"
+  echo "**********************************************************************"
+  echo "Site: $install ($install@$install.ssh.wpengine.net)"
   printf "\n"
   ssh $install@$install.ssh.wpengine.net 'cd /sites/$install; wp core update; wp core language update; wp theme update --all; wp plugin update --all --exclude=slider-pro,slider-pro-assets,mailgun,woocommerce; exit;'
+  echo "**********************************************************************"
+  printf "\n\n"
 done
+
+echo "**********************************************************************"
+echo "******************* YIKES Update Script Complete *********************"
+echo "**********************************************************************"
+printf "\n"
